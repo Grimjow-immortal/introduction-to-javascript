a2fd6f00d7926f7a995607201c51af0c03cf535c
Grimjow-immortal <Johndere46@gmail.com>
2022-06-23 22:27:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var myName = "ORITSEMEYIWA JOHN DERE";
+var myHeight = "6'2ft" ;
+var myCountry = "Nigeria";
+
+//console.log(myName);
+//console.log(myHeight);
+//console.log(myCountry);
+
+
+document.write(myName + "<br/> " + myHeight + " <br/>" + myCountry)
+
